a45a927b140177f6ebefca19c209c33062515dac
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2024-11-03 16:24:23 -0300

    Add files via upload

---
+++ Exercício_1172.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Exercicio1172
+{
+    static void Main()
+    {
+        int[] vetor = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+            if (vetor[i] <= 0)
+            {
+                vetor[i] = 1;
+            }
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"X[{i}] = {vetor[i]}");
+        }
+    }
+}
+
+
